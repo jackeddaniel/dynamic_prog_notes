d328212fd109faa21151d19a11676ca7577e290d
jackeddaniel <siddhant.ugarkar@gmail.com>
2026-02-10 20:59:27 +0530

    added tabulation for 0_1_knapsack

--- 0_1_knapsack.md
+++ 0_1_knapsack.md
@@ -32,6 +32,27 @@ int knapsack(vector<vector<int>>& t, vector<int>& wt, vector<int>& val, int w, i
 
 ## Tabulation Code
 ```cpp
+int knapsack(vector<int>& wt, vector<int>& val, int w, int n) {
+    int rows = n + 1;
+    int cols = w + 1;
+
+    vector<vector<int>> t(rows, vector<int>(cols, 0));
+
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < cols; j++) {
+            
+            if (wt[i - 1] <= j) {
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], 
+                              t[i - 1][j]);
+            } 
+            else {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+
+    return t[n][w];
+}
 ```
 
 
--- trial.cpp
+++ trial.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 using namespace std;
-
+/*
 int knapsack(vector<vector<int>>& t, vector<int>& wt, vector<int>& val, int w, int n) {
     if (n == 0 || w == 0) { 
         return 0;
@@ -16,4 +16,27 @@ int knapsack(vector<vector<int>>& t, vector<int>& wt, vector<int>& val, int w, i
     } else if (wt[n-1] > w) {
         return t[n][w] = knapsack(t, wt, val, w, n-1);
     }
+}
+*/
+
+int knapsack(vector<int>& wt, vector<int>& val, int w, int n) {
+    int rows = n + 1;
+    int cols = w + 1;
+
+    vector<vector<int>> t(rows, vector<int>(cols, 0));
+
+    for (int i = 1; i < rows; i++) {
+        for (int j = 1; j < cols; j++) {
+            
+            if (wt[i - 1] <= j) {
+                t[i][j] = max(val[i - 1] + t[i - 1][j - wt[i - 1]], 
+                              t[i - 1][j]);
+            } 
+            else {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+
+    return t[n][w];
 }
